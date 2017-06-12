7ca124687abb23f5c81402c62da497978d02815e
Ben Stacey <benstacey@MacBook-Pro.local>
2017-06-12 21:36:21 +0100

    first view added

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,17 @@
 var App = {
-
-
-
+	// allows access to App.templates.album (from View)
+	templates: JST,
+	$el: $("main"),
+	renderAlbums: function () {
+		this.albums.each(this.renderAlbumView);
+	},
+	renderAlbumView: function(album) {
+		debugger;
+		new AlbumView({
+			model: album,
+		})
+	},
+	init: function() {
+		this.renderAlbums();
+	}
 };
--- public/javascripts/views/album.js
+++ public/javascripts/views/album.js
@@ -1,4 +1,19 @@
 var AlbumView = Backbone.View.extend({
-	tagName: "li"
+
+	tagName: "li",
+	template: App.templates.album,
+	render: function() {
+		var id = this.model.get("id");
+
+		this.$el.attr("id", "album_" + id);
+		this.$el.html(this.template(this.model.toJSON()));
+		this.$el.appendTo(App.$el.find("ul"));
+	},
+	initialize: function() {
+		this.render();
+		this.model.view = this;																												// model 'back reference' so the model knows which view
+		
+	}																																								// is associated with it
 
 });
+
